72ad2ff919e0105b2b90dbe06f19e5a77a599702
ruvenij <ruvenij@gmail.com>
2015-11-16 23:06:54 +0530

    removed data bridge

--- src/main/java/org/mora/cep/main/Main.java
+++ src/main/java/org/mora/cep/main/Main.java
@@ -1,42 +1,21 @@
 package org.mora.cep.main;
 
-import org.mora.cep.cepProcessing.DataBridgeClient;
 import org.mora.cep.cepProcessing.DataFeed;
 import org.mora.cep.cepProcessing.MadisDataFeed;
-import org.mora.cep.reader.Consumer;
-
-import java.util.Timer;
 
 /**
  * Created by ruveni on 31/10/15.
  */
 public class Main {
     public static void main(String args[]) {
-        DataBridgeClient dataClient = new DataBridgeClient();
-        DataFeed dataFeed = new DataFeed(dataClient);
+        //radar data feed
+        DataFeed dataFeed = new DataFeed();
         Thread feedThread = new Thread(dataFeed);
         feedThread.start();
 
+        //madis data feed
         MadisDataFeed madisDataFeed=new MadisDataFeed();
         Thread madisThread=new Thread(madisDataFeed);
         madisThread.start();
-
-
-//        DataBridgeClient dataClient=new DataBridgeClient();
-//        TaskRepeat tr = new TaskRepeat(null, null);
-//        try{
-//            dataClient.SendDataToCEP();
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
-//
-        Timer timer = new Timer();
-        try {
-            timer.scheduleAtFixedRate(new Consumer(timer), 10000, 5000);
-        } catch (Exception e) {
-            timer.cancel();
-            timer.purge();
-            e.printStackTrace();
-        }
     }
 }
