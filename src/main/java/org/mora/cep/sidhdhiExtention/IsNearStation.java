999785047d328f90a3c4e362deb2bff7087e9511
ruvenij <ruvenij@gmail.com>
2015-12-08 12:55:09 +0530

    return type added

--- src/main/java/org/mora/cep/sidhdhiExtention/IsNearStation.java
+++ src/main/java/org/mora/cep/sidhdhiExtention/IsNearStation.java
@@ -18,7 +18,7 @@ public class IsNearStation extends FunctionExecutor {
     Attribute.Type returnType;
     private static final double DISTANCE_THRESHOLD = 100;
     private static final double EARTH_RADIUS = 6371;//in kilometers
-    private double latA,lonA,latB,lonB,dLat,dLng,a,c,dist;
+    private double latA = 0, lonA = 0, latB = 0, lonB = 0, dLat = 0, dLng = 0, a = 0, c = 0, dist = 0;
 
     @Override
     public void init(Attribute.Type[] types, SiddhiContext siddhiContext) {
@@ -36,21 +36,20 @@ public class IsNearStation extends FunctionExecutor {
     protected Object process(Object o) {
         boolean isNear = false;
         if ((o instanceof Object[]) && ((Object[]) o).length == 4) {
-            latA=Double.parseDouble(String.valueOf(((Object[]) o)[0]));
-            lonA=Double.parseDouble(String.valueOf(((Object[]) o)[1]));
-            latB=Double.parseDouble(String.valueOf(((Object[]) o)[2]));
-            lonB=Double.parseDouble(String.valueOf(((Object[]) o)[3]));
-
+            latA = Double.parseDouble(String.valueOf(((Object[]) o)[0]));
+            lonA = Double.parseDouble(String.valueOf(((Object[]) o)[1]));
+            latB = Double.parseDouble(String.valueOf(((Object[]) o)[2]));
+            lonB = Double.parseDouble(String.valueOf(((Object[]) o)[3]));
             dLat = Math.toRadians(latB - latA);
             dLng = Math.toRadians(lonB - lonA);
 
             //calculates the actual distance
-            a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(Math.toRadians(latA))* Math.cos(Math.toRadians(latB)) * Math.sin(dLng / 2) * Math.sin(dLng / 2);
+            a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(Math.toRadians(latA)) * Math.cos(Math.toRadians(latB)) * Math.sin(dLng / 2) * Math.sin(dLng / 2);
             c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-            dist = (double) (EARTH_RADIUS * c)*1000;
+            dist = (double) (EARTH_RADIUS * c) * 1000;
 
-            if(dist < DISTANCE_THRESHOLD){
-                isNear=true;
+            if (dist < DISTANCE_THRESHOLD) {
+                isNear = true;
             }
 
         }
@@ -64,6 +63,6 @@ public class IsNearStation extends FunctionExecutor {
 
     @Override
     public Attribute.Type getReturnType() {
-        return null;
+        return returnType;
     }
 }
