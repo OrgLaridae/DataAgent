5b6e3797179574fb641a21d60a45e9a5d9d6d04d
ruvenij <ruvenij@gmail.com>
2015-12-08 16:16:52 +0530

    Added CEPInitialize

--- src/main/java/org/mora/cep/main/Main.java
+++ src/main/java/org/mora/cep/main/Main.java
@@ -1,20 +1,24 @@
 package org.mora.cep.main;
 
+import org.mora.cep.cepProcessing.CEPInitialize;
 import org.mora.cep.cepProcessing.DataFeed;
 import org.mora.cep.cepProcessing.MadisDataFeed;
+import org.wso2.siddhi.core.SiddhiManager;
 
 /**
  * Created by ruveni on 31/10/15.
  */
 public class Main {
     public static void main(String args[]) {
+        CEPInitialize cep=new CEPInitialize();
+        SiddhiManager siddhiManager=cep.CEPInit();
         //radar data feed
-        DataFeed dataFeed = new DataFeed();
+        DataFeed dataFeed = new DataFeed(siddhiManager);
         Thread feedThread = new Thread(dataFeed);
         feedThread.start();
 
         //madis data feed
-        MadisDataFeed madisDataFeed=new MadisDataFeed();
+        MadisDataFeed madisDataFeed=new MadisDataFeed(siddhiManager);
         Thread madisThread=new Thread(madisDataFeed);
         madisThread.start();
     }
