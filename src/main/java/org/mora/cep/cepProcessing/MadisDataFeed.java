6f5ceba72b3a7d2d8fcc8a883c4ba6e4db8bf99e
ruvenij <ruvenij@gmail.com>
2015-12-08 12:54:41 +0530

    execution plan changed to remove temperature anomalies

--- src/main/java/org/mora/cep/cepProcessing/MadisDataFeed.java
+++ src/main/java/org/mora/cep/cepProcessing/MadisDataFeed.java
@@ -16,13 +16,15 @@ public class MadisDataFeed  implements Runnable{
     @Override
     public void run() {
         madisDataBridge=new MadisDataBridge();
+
+
         Path path = Paths.get("/home/ruveni/FYP/WRF-Data/chicago.csv");
         try (Stream<String> lines = Files.lines(path)) {
             String[] lineArray = lines.collect(Collectors.toList()).toArray(new String[0]);
             for (int i = 0; i < lineArray.length; i++) {
                 String[] stringData = lineArray[i].split(",");
                 try{
-                    madisDataBridge.SendDataToCEP(Double.parseDouble(stringData[5]),Double.parseDouble(stringData[6]),Double.parseDouble(stringData[7]),Double.parseDouble(stringData[8]),Double.parseDouble(stringData[9]),Double.parseDouble(stringData[10]),Double.parseDouble(stringData[11]),Double.parseDouble(stringData[12]),Double.parseDouble(stringData[13]));
+                    madisDataBridge.SendDataToCEP(stringData[0].trim(),stringData[1]+" "+stringData[2],Double.parseDouble(stringData[5]),Double.parseDouble(stringData[6]),Double.parseDouble(stringData[7]),Double.parseDouble(stringData[8]),Double.parseDouble(stringData[9]),Double.parseDouble(stringData[10]),Double.parseDouble(stringData[11]),Double.parseDouble(stringData[12]),Double.parseDouble(stringData[13]));
                 }catch(Exception e){
 
                 }
