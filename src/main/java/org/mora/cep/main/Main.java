f7b0c8de926eb54162c43c1b1030e9d93b83a0c5
ruvenij <ruvenij@gmail.com>
2015-12-27 23:06:26 +0530

    class name changes

--- src/main/java/org/mora/cep/main/Main.java
+++ src/main/java/org/mora/cep/main/Main.java
@@ -1,8 +1,9 @@
 package org.mora.cep.main;
 
 import org.mora.cep.cepProcessing.CEPInitialize;
-import org.mora.cep.cepProcessing.DataFeed;
+import org.mora.cep.cepProcessing.RadarDataFeed;
 import org.mora.cep.cepProcessing.MadisDataFeed;
+import org.mora.cep.cepProcessing.WeatherAlerts;
 import org.wso2.siddhi.core.SiddhiManager;
 
 /**
@@ -12,13 +13,15 @@ public class Main {
     public static void main(String args[]) {
         CEPInitialize cep=new CEPInitialize();
         SiddhiManager siddhiManager=cep.CEPInit();
+        WeatherAlerts weatherAlerts=new WeatherAlerts(siddhiManager);
+
         //radar data feed
-        DataFeed dataFeed = new DataFeed(siddhiManager);
+        RadarDataFeed dataFeed = new RadarDataFeed(weatherAlerts);
         Thread feedThread = new Thread(dataFeed);
         feedThread.start();
 
         //madis data feed
-        MadisDataFeed madisDataFeed=new MadisDataFeed(siddhiManager);
+        MadisDataFeed madisDataFeed=new MadisDataFeed(weatherAlerts);
         Thread madisThread=new Thread(madisDataFeed);
         madisThread.start();
     }
