2820c50cdfe494c8b0ec8838e369d5448491f064
ruvenij <ruvenij@gmail.com>
2016-01-21 01:36:38 +0530

    removed radar data file feeder

--- src/main/java/org/mora/cep/main/CEP.java
+++ src/main/java/org/mora/cep/main/CEP.java
@@ -2,35 +2,21 @@ package org.mora.cep.main;
 
 import org.mora.cep.cepProcessing.CEPInitialize;
 import org.mora.cep.dataFeed.CSVFileReader;
-import org.mora.cep.dataFeed.RadarDataFeed;
-import org.mora.cep.dataFeed.MadisDataFeed;
 import org.mora.cep.cepProcessing.WeatherAlerts;
 import org.wso2.siddhi.core.SiddhiManager;
 
 /**
  * Created by ruveni on 31/10/15.
  */
-public class Main {
+public class CEP {
     public static void main(String args[]) {
         CEPInitialize cep=new CEPInitialize();
         SiddhiManager siddhiManager=cep.CEPInit();
         WeatherAlerts weatherAlerts=new WeatherAlerts(siddhiManager);
-
-        //radar data feed
-        RadarDataFeed dataFeed = new RadarDataFeed(weatherAlerts);
-        Thread feedThread = new Thread(dataFeed);
-        feedThread.start();
-
-        //madis data feed
-//        MadisDataFeed madisDataFeed=new MadisDataFeed(weatherAlerts);
-//        Thread madisThread=new Thread(madisDataFeed);
-//        madisThread.start();
 
         //csv data feed - grib data
         CSVFileReader csvReader=new CSVFileReader(weatherAlerts);
         Thread csvThread=new Thread(csvReader);
         csvThread.start();
     }
-
-    //bhjgj
 }
