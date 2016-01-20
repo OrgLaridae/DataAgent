814ed7f101269c53aa5e92d34014282ca9e280e4
ruvenij <ruvenij@gmail.com>
2016-01-21 02:18:19 +0530

    adds a coordinate if it was not added before

--- src/main/java/org/mora/cep/sidhdhiExtention/LocationCoordinates.java
+++ src/main/java/org/mora/cep/sidhdhiExtention/LocationCoordinates.java
@@ -53,8 +53,9 @@ public class LocationCoordinates extends FunctionExecutor {
                 activatedAt=System.currentTimeMillis();
             }
 
-            //locationCoord=locationCoord+lat+":"+lon+",";
-            builder.append(lat+":"+lon+"\n");
+            if(builder.indexOf(lat+":"+lon)<0){
+                builder.append(lat+":"+lon+"\n");
+            }
         }
 
         //returns the calculated boundary
