45cdb462f43d7b9d69f6506f712eeef06837d9ab
ruvenij <ruvenij@gmail.com>
2016-01-24 03:06:40 +0530

    changed parameters

--- src/main/java/org/mora/cep/customEvents/AlertEvent.java
+++ src/main/java/org/mora/cep/customEvents/AlertEvent.java
@@ -1,18 +1,19 @@
 package org.mora.cep.customEvents;
 
+import java.util.ArrayList;
 import java.util.EventObject;
 
 /**
  * Created by ruveni on 1/8/16.
  */
 public class AlertEvent  extends EventObject{
-    private String location;
-    public AlertEvent(Object source, String coordinates){
+    private ArrayList<Location> location;
+    public AlertEvent(Object source, ArrayList<Location> coordinates){
         super(source);
         location=coordinates;
     }
 
-    public String getCoordinates(){
+    public ArrayList<Location> getCoordinates(){
         return location;
     }
 }
